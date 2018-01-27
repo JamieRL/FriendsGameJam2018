505c23c404f589b944c3fc330f5886403f4e1597
JamieRL <lee.jamr@gmail.com>
2018-01-26 22:08:17 -0800

    change movement scripts, add prefab folder

---
+++ Assets/scripts/TopDownCharacterMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// © 2017 TheFlyingKeyboard and released under MIT License
+// theflyingkeyboard.net
+public class TopDownCharacterMovement : MonoBehaviour {
+	private Rigidbody2D myRigidbody;
+	[SerializeField] private float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 movingVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		myRigidbody.AddForce(movingVector * moveSpeed);
+	}
+}
