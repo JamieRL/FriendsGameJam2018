6103369a87a7c5cc89e13b31ef025461253c9293
Yasmeen Akbari <yasmeen.akbari@gmail.com>
2018-01-27 10:19:15 -0800

    Added minimum follow distance to the chaser script

--- Assets/scripts/Chaser.cs
+++ Assets/scripts/Chaser.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Chaser : MonoBehaviour {
 
     public Transform m_target;
-    public float m_moveSpeed;
+    public float m_moveSpeed = 3;
+    public float m_minFollowDistance = 0;
 
     private Rigidbody2D m_rigidbody2d;
 
@@ -19,12 +20,19 @@ public class Chaser : MonoBehaviour {
 
         Vector3 dirToTarget = m_target.position - transform.position;
         Vector3 normalizedDirToTarget = Vector3.Normalize(dirToTarget);
+        float distance = Vector3.Distance(transform.position, m_target.position);
 
         // Rotate to face the target
         float angle = Mathf.Atan2(dirToTarget.y, dirToTarget.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        
-        m_rigidbody2d.velocity = new Vector2(normalizedDirToTarget.x * m_moveSpeed, normalizedDirToTarget.y * m_moveSpeed);
 
+        if (distance > m_minFollowDistance)
+        {
+            m_rigidbody2d.velocity = new Vector2(normalizedDirToTarget.x * m_moveSpeed, normalizedDirToTarget.y * m_moveSpeed);
+        }
+        else
+        {
+            m_rigidbody2d.velocity = new Vector2(0, 0);
+        }
 	}
 }
