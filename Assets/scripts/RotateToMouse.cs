65ca5e27007fc30428d86e0ec8028a6870e2a1fe
JamieRL <lee.jamr@gmail.com>
2018-01-28 08:45:48 -0800

    add damage to enemy, fix player death explosion

--- Assets/scripts/RotateToMouse.cs
+++ Assets/scripts/RotateToMouse.cs
@@ -19,9 +19,12 @@ public class RotateToMouse : MonoBehaviour {
                 return;
             }
         }
-		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-		difference.Normalize();
-		float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset);
+        if (GetComponent<Health>().isAlive)
+        {
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            difference.Normalize();
+            float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset);
+        }
 	}
 }
