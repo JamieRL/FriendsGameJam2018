ca37fdbe91bb2ba8871f20633da317ffb95272c2
JamieRL <lee.jamr@gmail.com>
2018-01-27 16:12:20 -0800

    add player animator controller

--- Assets/scripts/PlayerUserController.cs
+++ Assets/scripts/PlayerUserController.cs
@@ -7,13 +7,33 @@ public class PlayerUserController : MonoBehaviour {
 
     private ShooterProjectile shooter;
 
+    private Animator animator;
+
 	// Use this for initialization
 	void Start () {
         shooter = GetComponent<ShooterProjectile>();
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        
+        if (GameManager.gm)
+        {
+            if (GameManager.gm.gameIsOver)
+            {
+                return;
+            }
+        }
+
+        if(Input.GetButton("Horizontal") || Input.GetButton(("Vertical"))) {
+            Debug.Log("setting animator to boosting");
+            animator.SetBool("isBoosting", true);
+        }
+        else {
+            Debug.Log(("setting animator to not boosting"));
+            animator.SetBool("isBoosting", false);
+        }
 		if (Input.GetButton("Fire1"))
         {
             shooter.Shoot();
