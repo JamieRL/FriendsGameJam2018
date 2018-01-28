0aa29e5491d259febc4b4cb5a4378e4f7f367f6f
Mattman980 <matthew.clark007@gmail.com>
2018-01-27 19:25:46 -0800

    Moved Character, Mixed Sound, Changed Text
    see title

--- Assets/scripts/EngineNoise.cs
+++ Assets/scripts/EngineNoise.cs
@@ -22,12 +22,12 @@ public class EngineNoise : MonoBehaviour
     // Update is called once per frame
     void Update() {
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical")) {
-            movingAudio.volume = 0.7f;
+            movingAudio.volume = 0.2f;
             stationaryAudio.volume = 0.0f;
         }
         else
         {
-            stationaryAudio.volume = 0.3f;
+            stationaryAudio.volume = 0.1f;
             movingAudio.volume = 0.0f;
         }
 
